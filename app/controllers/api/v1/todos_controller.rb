867379c87a4dd8cf581b9d26ba4fcf6cdc1a0375
yamatoto <mftb35ymt@yahoo.co.jp>
2020-11-01 09:01:46 +0900

    ディレクトリ移動

---
+++ app/controllers/api/v1/todos_controller.rb
@@ -0,0 +1,60 @@
+module Api
+  module V1
+    class TodosController < ApplicationController
+      before_action :set_todo, only: [:show, :update, :destroy]
+
+      # GET /todos
+      def index
+        @todos = Todo.all
+
+        render json: @todos
+      end
+
+      # GET /todos/1
+      def show
+        render json: @todo
+      end
+
+      # POST /todos
+      def create
+        # @todo = Todo.new(todo_params)
+        todo = Todo.new(todo_params)
+
+        #if @todo.save
+        if todo.save
+            render json: { status: 'SUCCESS', data: todo }
+          # render json: @todo, status: :created, location: @todo
+        else
+          render json: { status: 'ERROR', data: todo.errors }
+          # render json: @todo.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /todos/1
+      def update
+        if @todo.update(todo_params)
+          render json: @todo
+        else
+          render json: @todo.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /todos/1
+      def destroy
+        @todo.destroy
+      end
+
+      private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_todo
+          @todo = Todo.find(params[:id])
+        end
+
+        # Only allow a trusted parameter "white list" through.
+        def todo_params
+          params.permit(:title, :content)
+          # params.fetch(:todo, {}).permit(:title, :content, :category_id, :post_user_id)
+        end
+    end
+  end
+end
