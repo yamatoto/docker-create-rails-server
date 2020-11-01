32692de4830f7247e7fcd75669f8d566d8f3a7a7
yamatoto <mftb35ymt@yahoo.co.jp>
2020-11-01 09:02:04 +0900

    todos model修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_18_045921) do
+ActiveRecord::Schema.define(version: 2020_10_18_123704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 2020_10_18_045921) do
   create_table "todos", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.string "content"
+    t.integer "category_id"
+    t.string "post_user_id"
   end
 
 end
